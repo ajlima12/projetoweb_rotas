976014533f98a9ee6081e86459cd1007e92bdcaf
ajlima12 <ajlimaa12@gmail.com>
2023-02-24 17:50:24 -0300

    Aula sobre rotas

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+</head>
+<body>
+<a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a><br><br>
+<a href="{{ url('/empresa') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Empresa</a>
+
+<background-color: black>
+</body>
+</html>
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a><br><br>
+<a href="{{ url('/produtos') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Produtos</a>
+
+<background-color: red>
+</body>
+</html>
